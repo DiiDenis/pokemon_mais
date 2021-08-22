8279cfae1ceb598c249fb730c16fe0153c44cad3
Denis Izaias Pereira <diidenis@hotmail.com>
2021-08-22 13:58:36 -0300

    teste

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'pokemon_mais',
+    title: 'pokémon_mais',
     htmlAttrs: {
       lang: 'en'
     },
